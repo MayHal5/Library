8d9965f6c554d0164a82a8d944c54ffee2d2e031
Your Name <you@example.com>
2024-03-04 11:47:53 +0200

    Revert "finish the text reader"
    This reverts commit 4ec70cbce3f4db51a8a81025740c9113de996522.

--- main.py
+++ main.py
@@ -3,17 +3,14 @@
     Author: Maysan Halaby
     Date: 03/03/2024
 """
-import file_types.text_reader
-from file_types.file_read.file_read import FileRead
-from file_types.text_reader import TextReader
+import file_read
 
 
 def main():
     """
     Entry point
     """
-    file_path = file_types.text_reader.TextReader(r"C:\Users\User\Desktop\hellooo.txt")
-    file_path.read()
+    file_read.FileRead(r"C:\Users\User\Documents\ippp.txt")
 
 
 if __name__ == '__main__':
